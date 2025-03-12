015490d99943a00a2d26e263c4c62c6a7f21a768
chand-1907 <120355208+chand-1907@users.noreply.github.com>
2025-03-12 22:21:43 +0530

    Update trick.py

--- trick.py
+++ trick.py
@@ -11,3 +11,16 @@ print(c + d == e)
 x = True + True + False
 y = True + False + False
 print(x, y)
+a = [1, 2, 3]
+b = a
+b.append(4)
+print(a)
+#added the comment - 1
+c=0.1
+d=0.2
+e=0.3
+print(c + d == e)
+#added the comment - 2
+x = True + True + False
+y = True + False + False
+print(x, y)
